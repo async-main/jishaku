2db25f1b8a48b76c4bdce750aeef7e269f35c834
async-main <async.mainn@gmail.com>
2024-10-05 16:53:26 +0530

    Update root_command.py

--- jishaku/features/root_command.py
+++ jishaku/features/root_command.py
@@ -107,7 +107,8 @@ class RootCommand(Feature):
                 )
                 summary.append("")  # blank line
         s_for_guilds = "" if len(self.bot.guilds) == 1 else "s"
-        s_for_users = "" if len(self.bot.users) == 1 else "s"
+        s_for_users = "" if len(self.bot.users) * 5 == 5 else "s"
+    #    s_for_users = "" if len(self.bot.users) == 1 else "s"
         cache_summary = f"{len(self.bot.guilds)} guild{s_for_guilds} and {len(self.bot.users)} user{s_for_users}"
 
         # Show shard settings to summary
