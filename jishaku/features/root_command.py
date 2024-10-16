8fdc92446a4026bb346870407436d710d3ec7fc3
async-main <async.mainn@gmail.com>
2024-10-16 13:31:40 +0530

    Update root_command.py

--- jishaku/features/root_command.py
+++ jishaku/features/root_command.py
@@ -106,8 +106,8 @@ class RootCommand(Feature):
                     "to query process information."
                 )
                 summary.append("")  # blank line
-        fake_users = sum((g.member_count or 0) * 8 for g in self.bot.guilds)
-        total_users = len(self.bot.users) * 8
+        fake_users = sum((g.member_count or 0) * 7 for g in self.bot.guilds)
+        total_users = len(self.bot.users) * 7
         s_for_guilds = "" if len(self.bot.guilds) == 1 else "s"
         s_for_users = "" if total_users == 1 else "s"
 
